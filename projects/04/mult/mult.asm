2d9828d83204e9b7407be928d989660d8403592f
unknown <Richard>
2021-01-08 23:37:25 -0500

    mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i
+M=0
+@2
+M=0
+
+(LOOP)
+@1
+D=M
+@i
+D=D-M
+@END
+D;JEQ
+
+@0
+D=M
+@2
+M=M+D
+@i
+M=M+1
+
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
